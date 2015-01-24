003aafcbcc7a8488b3c1beca5db2ae7197493f48
Andrei Taylor <andreirtaylor@gmail.com>
2015-01-24 13:39:45 -0800

    Added jasmine and sockets to commit.

--- public/javascripts/moneyTracker.js
+++ public/javascripts/moneyTracker.js
@@ -1,14 +1,15 @@
-var myApp = angular.module('App', []);
+(function(){
+        var MT = angular.module('App', []);
 
-myApp.controller('MT', function ($scope) {
-      $scope.something = "thiswillprint";
-      console.log("hadjafdlj");
-      $scope.phones = [
-        {'name': 'Nexus S',
-                 'snippet': 'Fast just got faster with Nexus S.'},
-        {'name': 'Motorola XOOM™ with Wi-Fi',
-                 'snippet': 'The Next, Next Generation tablet.'},
-        {'name': 'MOTOROLA XOOM™',
-                 'snippet': 'The Next, Next Generation tablet.'}
-  ];
-});
+        MT.controller('MT', function ($scope) {
+              $scope.something = socket.game ;
+              $scope.phones = [
+                {'name': 'Nexus S',
+                         'snippet': 'Fast just got faster with Nexus S.'},
+                {'name': 'Motorola XOOM™ with Wi-Fi',
+                         'snippet': 'The Next, Next Generation tablet.'},
+                {'name': 'MOTOROLA XOOM™',
+                         'snippet': 'The Next, Next Generation tablet.'}
+          ];
+        });
+})()
