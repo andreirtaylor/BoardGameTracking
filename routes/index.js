541bcea018f1008e68b8faac5db2b2cb9b739ac6
User <User@Users-MacBook-Pro-2.local>
2015-01-31 11:56:36 -0800

    /addScreen added

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/addScreen', function(req, res, next){
+	res.render('addScreen' {title: 'Test calculator'});
+});
+
 module.exports = router;
