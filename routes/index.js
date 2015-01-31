ac6224eecd34bc6ce228230887c38ee4b39a43bf
User <User@Users-MacBook-Pro-2.local>
2015-01-31 12:06:18 -0800

    /addScreen works

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
 });
 
 router.get('/addScreen', function(req, res, next){
-	res.render('addScreen' {title: 'Test calculator'});
+	res.render('addScreen', {title: 'Test calculator'});
 });
 
 module.exports = router;
