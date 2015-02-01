63a7b021fbb27e7a863bbfcfeea8e652c0c2bb1c
Andrei Taylor <andreirtaylor@gmail.com>
2015-01-31 16:06:49 -0800

    Moved the socket logic out of the www file (mostly for the ablilty to do syntax highlighting

---
+++ bin/socketsServ.js
@@ -0,0 +1,97 @@
+module.exports = function(io) {
+    var gamesList = {
+        sampleGame:{
+            playerList:
+            [
+                {
+                    name: "Andrei",
+                    money: 123.34
+                },
+                {
+                    name: "Jonah",
+                    money: -100000000.0
+                },
+                {
+                    name: "Paul",
+                    money: 0937.80
+                },
+                {
+                    name: "Jason",
+                    money: 80085.69
+                 },
+                 {
+                    name: "Chris",
+                    money: 0.00
+                 }
+            ],
+            gameName: "sampleGame",
+            numberOfPlayers: 5
+        },
+        testConnection:{
+            playerList:
+            [
+                {
+                    name: "Andrei",
+                    imoney: 100.00,
+                    money: 100
+                },
+                {
+                    name: "Jonah",
+                    imoney: -100000000.0,
+                    money: -100000000.0
+                },
+                {
+                    name: "Paul",
+                    imoney: 937.80,
+                    money: 937.80
+                },
+                {
+                    name: "Jason",
+                    imoney: 80085.69,
+                    money: 80085.69
+                 },
+                 {
+                    name: "Chris",
+                    imoney: 1.00,
+                    money: 1.00
+                 }
+            ],
+            gameName: "sampleGame",
+            numberOfPlayers: 5
+        }
+    };
+
+    // socket things
+    io.on('connection', function (socket) {
+        socket.emit('gameStart', gamesList["sampleGame"]);
+        
+        socket.on('my other event', function (data) {
+            console.log(data);
+        });
+
+        // can you send multiple things over the socket?
+        // generates random ammounts of money and sends them to the client endlessly
+        var testConnection = function(game){
+            var players = gamesList["testConnection"]["playerList"];
+            console.log(players);
+            for (player in players){
+                if(players.hasOwnProperty(player)){
+                    players[player]['money'] = Math.random()*players[player]['imoney'];
+                    console.log(players[player]['money']);
+                }    
+            }             
+            socket.emit('testConnection', game)
+            console.log("Sending Socket");
+        };
+        
+        // use this to test your connection, if you see the game money updating
+        // you are in good shape
+        setInterval(function(){
+            testConnection(gamesList["testConnection"]);
+            }, 2000);
+        
+        socket.on('updateGameData', function(newGameData){ 
+                    
+        })
+    });
+}
