6575ccff1eb124e475574899f06f956f637251ed
Andrei Taylor <andreirtaylor@gmail.com>
2015-03-02 17:40:56 -0800

    added in a new file

---
+++ bin/gamesList.js
@@ -0,0 +1,75 @@
+// these are some sample games, very likely that the format
+// of these games will change so dont get too attached
+module.exports = {
+    samplegame:{
+        playerList:
+        [
+            {
+                id:0,
+                name: "Andrei",
+                money: 50
+            },
+            {
+                id:1,
+                name: "Jonah",
+                money: 50
+            },
+            {
+                id:2,
+                name: "Paul",
+                money: 50
+            },
+            {
+                id:3,
+                name: "Jason",
+                money: 50 
+             },
+             {
+                id:4,
+                name: "Chris",
+                money: 50
+             }
+        ],
+        startMoney:50,
+        gameName: "samplegame",
+        numberOfPlayers: 5
+    },
+    testConnection:{
+        playerList:
+        [
+            {
+                id:0,
+                name: "Andrei",
+                imoney: 100.00,
+                money: 100
+            },
+            {
+                id:1,
+                name: "Jonah",
+                imoney: -100000000.0,
+                money: -100000000.0
+            },
+            {
+                id:2,
+                name: "Paul",
+                imoney: 937.80,
+                money: 937.80
+            },
+            {
+                id:3,
+                name: "Jason",
+                imoney: 80085.69,
+                money: 80085.69
+             },
+             {
+                id:4,
+                name: "Chris",
+                imoney: 1.00,
+                money: 1.00
+             }
+        ],
+        gameName: "sampleGame",
+        numberOfPlayers: 5
+    }
+}
+
