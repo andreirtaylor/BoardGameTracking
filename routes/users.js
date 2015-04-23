42bd9330dceb5884bd78440e5d50b83c0daf3c38
Adam Schmidt <adamtheschmidt@hotmail.com>
2015-04-22 19:47:51 -0700

    the new regex for emails

--- routes/users.js
+++ routes/users.js
@@ -100,7 +100,7 @@ router.post('/register', function(req, res, next) {
             type: "danger"});
     }
     //check for valid email
-    var re = /^\S+@\S+$/
+    var re = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i 
     if(!re.test(email)){
         return res.render('register',{
             message:"Email not valid",
